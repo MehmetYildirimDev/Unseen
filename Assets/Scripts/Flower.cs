82007524f6418460dd7a10baea6d442ce99f0768
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2023-03-25 01:42:10 +0300

    feat: create flower script

---
+++ Assets/Scripts/Flower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Can verildi");
+            collision.transform.localScale += Vector3.one; 
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("kendimi yok ettim");
+    }
+}
