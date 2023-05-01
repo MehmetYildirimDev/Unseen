b5d757c476be241c630ae0c76589dbcec353285b
Mehmet Yıldırım <mehmetyildirim.dev@gmail.com>
2023-05-01 18:54:16 +0300

    feat: create new animation for portal exit cover

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,11 +6,13 @@ public class Portal : MonoBehaviour
 {
     private Transform SpawnPoint;
     private GameObject PortalExit;
+    private GameObject PortalCover;
     [SerializeField] private GameObject EffectPrefab;
     private void Start()
     {
         PortalExit = GameObject.Find("PortalExit");
         SpawnPoint = PortalExit.transform.GetChild(0).transform;
+        PortalCover = PortalExit.transform.GetChild(3).gameObject;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -19,9 +21,12 @@ public class Portal : MonoBehaviour
         {
             other.transform.position = SpawnPoint.position;
 
+            PortalCover.GetComponent<Animation>().Play("PortalExitCoverAnim");
+
             GameObject Effect = Instantiate(EffectPrefab,
                 new Vector3(SpawnPoint.position.x,SpawnPoint.position.y+2,SpawnPoint.position.z),
                 Quaternion.identity);
+
 
             Destroy(Effect, 1f);
         }
