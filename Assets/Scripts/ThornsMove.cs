f2551a4a3d39d339c15c6c3aac8118056e33987a
Mehmet Yıldırım <mehmetyildirim.dev@gmail.com>
2023-05-03 22:00:29 +0300

    feat: level design for level 16

--- Assets/Scripts/ThornsMove.cs
+++ Assets/Scripts/ThornsMove.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class ThornsMove : MonoBehaviour
 {
     public float Rotatespeed = 3f;
-    public float Movespeed = 4f;
 
     void Update()
     {
         transform.Rotate(Vector3.up, Rotatespeed);
-
     }
 
     private void OnTriggerEnter(Collider other)
