762348216a10caa6a048a6616237f562f7c13bb5
Mehmet Yıldırım <mehmetyildirim.dev@gmail.com>
2023-05-04 18:39:15 +0300

    fix: bug fix for Guillotine, Portal, LockDoorSystem

--- Assets/Scripts/LockDoor.cs
+++ Assets/Scripts/LockDoor.cs
@@ -7,6 +7,8 @@ public class LockDoor : MonoBehaviour
     private GameObject[] doors = new GameObject[2];
     //bu objenin collideri kapanicak doorslarýn collideri acilicak => anahtarý alinca
 
+    public Material defultDoorMat;
+
     private void Start()
     {
         doors[0] = transform.GetChild(0).gameObject;
@@ -15,14 +17,16 @@ public class LockDoor : MonoBehaviour
 
     public void OpenLockDoor()
     {
+        print("open the door");
         GetComponent<BoxCollider>().enabled = false;
         foreach (var item in doors)
         {
             item.GetComponent<BoxCollider>().enabled = true;
-            item.GetComponent<MeshRenderer>().materials[1].color = new Color (108,223,130);
+            item.GetComponent<MeshRenderer>().materials[1].color = defultDoorMat.color;
+
         }
 
-        
+
 
     }
 
