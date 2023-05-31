36ec28be49c630dc622e3b1ff56117a77fc58087
Mehmet Yıldırım <mehmetyildirim.dev@gmail.com>
2023-05-31 23:52:50 +0300

    feat: create info panel

--- Assets/Scripts/Menus/mainMenu.cs
+++ Assets/Scripts/Menus/mainMenu.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+
+    public GameObject InfoPanel;
+
     private void Start()
     {
         MenuManager.IsInitialised = false;
+        InfoPanel.SetActive(false);
     }
 
     public void OnClick_Settings()
@@ -23,6 +28,26 @@ public class mainMenu : MonoBehaviour
     public void OnClick_Play()
     {
         SceneManager.LoadScene("LevelSelection");
+    }
+
+
+    public List<Text> texts = new List<Text>();
+    public void Onclick_Info()
+    {
+        if (InfoPanel.activeSelf == true)
+        {
+            InfoPanel.SetActive(false);
+
+            return;
+        }
+        //TODO: firebaseden verileri oku
+
+        for (int i = 0; i < FirebaseManager.instance.data.Count; i++)
+        {
+            texts[i].text = FirebaseManager.instance.data[i];
+        }
+
+        InfoPanel.SetActive(true);
     }
 
     public void onClick_Exit()
